be8038ea1d9ca4d34738c0af90aff1066926190a
Pusztai S Krisztian <info@pusztasa.eu>
2025-11-24 18:36:08 +0100

    vanNyertes metodus hozzaadasa

--- src/main/java/amoba/Tabla.java
+++ src/main/java/amoba/Tabla.java
@@ -32,7 +32,7 @@ public class Tabla {
             }
             System.out.println("+");
 
-            System.out.printf("%2d ", sor + 1); // Sor száma
+            System.out.printf("%2d ", sor + 1);
             for (int oszlop = 0; oszlop < meret; oszlop++) {
                 System.out.print("| " + mezok[sor][oszlop] + " ");
             }
@@ -45,4 +45,85 @@ public class Tabla {
         }
         System.out.println("+");
     }
+
+    public boolean lepes(int sor, int oszlop, char jel) {
+        if (sor < 0 || sor >= meret || oszlop < 0 || oszlop >= meret) {
+            return false;
+        }
+        if (mezok[sor][oszlop] != ' ') {
+            return false;
+        }
+        mezok[sor][oszlop] = jel;
+        return true;
+    }
+
+    public List<int[]> getSzabadmezok() {
+        List <int[]> szabadMezok = new ArrayList<>();
+
+        for (int sor = 0; sor < meret; sor++) {
+            for (int oszlop = 0; oszlop < meret; oszlop++) {
+                if (mezok[sor][oszlop] == ' ') {
+                    szabadMezok.add(new int[]{sor, oszlop});
+                }
+            }
+        }
+        return szabadMezok;
+    }
+
+    public boolean vanNyertes(char jel) {
+        for (int sor = 0; sor < meret; sor++) {
+            int db = 0;
+
+            for (int oszlop = 0; oszlop < meret; oszlop++) {
+                if (mezok[sor][oszlop] == jel) {
+                    db++;
+                    if (db == 5)
+                        return true;
+                }   else {
+                    db = 0;
+                }
+            }
+        }
+
+        for (int oszlop = 0; oszlop < meret; oszlop++) {
+            int db = 0;
+            for (int sor = 0; sor < meret; sor++) {
+                if (mezok[sor][oszlop] == jel) {
+                    db++;
+                    if (db == 5)
+                        return true;
+                }   else {
+                    db = 0;
+                }
+            }
+        }
+
+        for (int sor = 0; sor <= meret - 5; sor++) {
+            for (int oszlop = 0; oszlop <= meret - 5; oszlop++) {
+                int db = 0;
+                for (int k = 0; k < 5; k++) {
+                    if (mezok[sor + k][oszlop + k] == jel) {
+                        db++;
+                        if (db == 5)
+                            return true;
+                    }   else break;
+                }
+            }
+        }
+
+        for (int sor = 0; sor <= meret - 5; sor++) {
+            for (int oszlop = 4; oszlop < meret; oszlop++) {
+                int db = 0;
+                for (int k = 0; k < 5; k++) {
+                    if (mezok[sor + k][oszlop - k] == jel) {
+                        db++;
+                        if (db == 5)
+                            return true;
+                    }   else break;
+                }
+            }
+        }
+        return false;
+    }
+
 }
