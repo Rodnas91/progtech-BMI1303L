755f2e0d3743490a5361f50be5dd6ee6da2169de
Pusztai S Krisztian <info@pusztasa.eu>
2026-01-01 19:38:45 +0100

    Teszt osztály javítva és beállítva

---
+++ src/test/java/amoba/TablaTest.java
@@ -0,0 +1,29 @@
+package amoba;
+import amoba.Tabla;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TablaTest {
+
+    @Test
+    public void testUresMezoErvenyes() {
+        Tabla tabla = new Tabla(5);
+        assertTrue(tabla.lepes(0, 0, 'X'));
+    }
+
+    @Test
+    public void testFoglaltMezoErvenytelen() {
+        Tabla tabla = new Tabla(5);
+        tabla.lepes(0, 0, 'X');
+        assertFalse(tabla.lepes(0, 0, 'O'));
+    }
+
+    @Test
+    public void testVanNyertesSorban() {
+        Tabla tabla = new Tabla(5);
+        for (int i = 0; i < 5; i++) {
+            tabla.lepes(0, i, 'X');
+        }
+        assertTrue(tabla.vanNyertes('X'));
+    }
+}
